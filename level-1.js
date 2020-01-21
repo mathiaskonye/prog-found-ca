d06ab17f961d377a506080662de0d1a5f97143b1
mathiaskonye <56630416+mathiaskonye@users.noreply.github.com>
2020-01-21 22:45:47 +0100

    Update level-1.js

--- level-1.js
+++ level-1.js
@@ -1,10 +1,13 @@
+// Question 1
+
 var humanFashon = "This human needs some yellow sneakers";
 
+// Question 2
 var person = {
     name: "stewart",
     age: 37
 };
-
+// Question 3
 var outOfStock = true
 
 if (outOfStock === true) {
@@ -14,20 +17,78 @@ else if (outOfstock === false) {
     console.log("In stock");
 }
 
-var shoeSize = ["40","41","42","43","44"];
-var firstShoeSize = shoeSize[0];
-var secondShoeSize = shoeSize[1];
-var thirdShoeSize = shoeSize[2];
-var fourthShoeSize = shoeSize[3];
-var fifthShoeSize = shoeSize[4];
+// Question 4
 
-console.log(shoeSize[0]);
-console.log(shoeSize[1]);
-console.log(shoeSize[2]);
-console.log(shoeSize[3]);
-console.log(shoeSize[4]);
+var shoeSize = [40,41,42,43,44];
+
+for (var i=0; i <=shoeSize.length; i++){
+    console.log(shoeSize[i]);
+};
+
+// Question 5
 
 for (var count = 15; count <= 25; count++) {
     console.log(count);
 }
+
+// Question 6
+
+for (var count = 15; count <= 25; count++) {
+    if (count === 20) {
+        console.log(count);
+    }
+}
+
+// Question 7
+
+var shoes=[ 
+    {
+        name:"adidas",
+        size:43,
+        outOfStock:true,
+    },
+    {
+        name:"Nike",
+        size:43,
+        outOfStock:false,
+    }
+]
+
+for (var i = 0; i < shoes.length; i++) {
+    console.group(shoes[i].size);
+    console.group(shoes[i].outOfStock);
+}
+
+
+// Question 8
+
+function whatIdontLike(arg) {
+    console.log("I dont like",arg);
+}
+whatIdontLike("Old shoes");
+
+// Question 9
+
+function shoeSize(size1, size2) {
+    return size1 - size2;
+};
+shoeSize(80,42);
+
+// Question 10
+
+var newShoes = [];
+
+function putInArray(stuff){
+    newShoes.push(stuff);
+}
+
+putInArray("Brand new shoes!");
+
+console.log(newShoes);
+
+
+
+
+
+
 

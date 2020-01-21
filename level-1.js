783b4ae367628e5380c8c49c97ee763df44b4783
mathiaskonye <56630416+mathiaskonye@users.noreply.github.com>
2020-01-21 10:27:27 +0100

    Create level-1.js

---
+++ level-1.js
@@ -0,0 +1,33 @@
+var humanFashon = "This human needs some yellow sneakers";
+
+var person = {
+    name: "stewart",
+    age: 37
+};
+
+var outOfStock = true
+
+if (outOfStock === true) {
+    console.log("Out of stock");
+}
+else if (outOfstock === false) {
+    console.log("In stock");
+}
+
+var shoeSize = ["40","41","42","43","44"];
+var firstShoeSize = shoeSize[0];
+var secondShoeSize = shoeSize[1];
+var thirdShoeSize = shoeSize[2];
+var fourthShoeSize = shoeSize[3];
+var fifthShoeSize = shoeSize[4];
+
+console.log(shoeSize[0]);
+console.log(shoeSize[1]);
+console.log(shoeSize[2]);
+console.log(shoeSize[3]);
+console.log(shoeSize[4]);
+
+for (var count = 15; count <= 25; count++) {
+    console.log(count);
+}
+
